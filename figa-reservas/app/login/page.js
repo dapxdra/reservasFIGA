70168e7954da26e25c7a1e4dfb2e016db3216b3e
Alejandro Carvajal <ale.carva16@hotmail.com>
2025-02-17 21:56:09 -0600

    working on login interface

--- figa-reservas/app/login/page.js
+++ figa-reservas/app/login/page.js
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import { auth } from "../lib/firebase.js";
 import Image from "next/image";
 
+
 export default function LoginPage() {
   const router = useRouter();
   const [email, setEmail] = useState("");
@@ -26,49 +27,51 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-96 ">
-        <Image
-          src="/logo.PNG"
-          alt="FIGA Logo"
-          width={96}
-          height={96}
-          className="flex justify-center items-center mx-auto mb-4"
-        />
+    <div className="grid grid-cols-1 place-items-center h-screen bg-gray-100">
 
-        {error && <p className="text-red-500 text-center mb-2">{error}</p>}
+      <Image
+        src="/logo.PNG"
+        alt="FIGA Logo"
+        width={200}
+        height={200}
+        className=""
+      />
 
-        <form onSubmit={handleLogin} className="space-y-4">
-          <div>
-            <label className="block text-gray-700">Email</label>
-            <input
-              type="email"
-              placeholder=""
-              className="w-full p-2 border border-gray-300 rounded mt-1"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
+      <div id="divlogin" className="border-1 border-black">
 
-          <div>
-            <label className="block text-gray-700">Password</label>
-            <input
-              type="password"
-              placeholder=""
-              className="w-full p-2 border border-gray-300 rounded mt-1"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
+        {error && <p className="text-red-500 mb-2">{error}</p>}
 
-          <button
-            type="submit"
-            className="w-full bg-black text-white py-2 rounded hover:bg-gray-800"
-          >
-            Sign in
-          </button>
+        <form onSubmit={handleLogin} className="space-y-4 text-black">
+            <div>
+              <label className="text-gray-700">Email</label>
+              <input
+                type="email"
+                placeholder="Type your email"
+                className="w-full p-2 border border-gray-300 rounded mt-1"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+
+            <div>
+              <label className="text-gray-700">Password</label>
+              <input
+                type="password"
+                placeholder="Type your password"
+                className="w-full p-2 border border-gray-300 rounded mt-1"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-black text-white py-2 rounded hover:bg-gray-800"
+            >
+              Sign in
+            </button>
         </form>
       </div>
     </div>
--- figa-reservas/postcss.config.mjs
+++ figa-reservas/postcss.config.mjs
@@ -1,4 +1,13 @@
 const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+   
+   // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+   ],   
+
   plugins: {
     "@tailwindcss/postcss": {},
   },
