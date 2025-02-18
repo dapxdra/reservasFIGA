61ffc0b4bb3b575e9c6df686f0f89e4cdac16d20
dapxdra <da.araya98@gmail.com>
2025-02-17 19:54:15 -0600

    cambios en el la carpeta lib

--- figa-reservas/app/api/reservas/route.js
+++ figa-reservas/app/api/reservas/route.js
@@ -1,11 +1,8 @@
-import { db } from "../../lib/firebase.js";
+import { db } from "../../lib/firebaseadmin.js";
 
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Método no permitido" });
-  }
-
+export const POST = async (req) => {
   try {
+    const body = await req.json();
     const reservasRef = db.collection("reservas");
 
     // Obtener el último ID registrado
@@ -22,25 +19,33 @@ export default async function handler(req, res) {
     // Crear la nueva reserva con ID incremental
     const newReserva = {
       figaId: newId,
-      itinId: req.body.itinId || 0,
-      cliente: req.body.cliente || "",
-      fecha: req.body.fecha || "",
-      hora: req.body.hora || "",
-      dropOff: req.body.dropOff || "",
-      pickUp: req.body.pickUp || "",
-      proveedor: req.body.proveedor || "",
-      nota: req.body.nota || "",
-      precio: req.body.precio || 0,
-      AD: req.body.AD || 0,
-      NI: req.body.NI || 0,
+      itinId: body.itinId || 0,
+      cliente: body.cliente || "",
+      fecha: body.fecha || "",
+      hora: body.hora || "",
+      dropOff: body.dropOff || "",
+      pickUp: body.pickUp || "",
+      proveedor: body.proveedor || "",
+      nota: body.nota || "",
+      precio: body.precio || 0,
+      AD: body.AD || 0,
+      NI: body.NI || 0,
       createdAt: new Date(),
     };
 
     await reservasRef.doc(newId.toString()).set(newReserva);
 
-    return res.status(200).json({ message: "Reserva creada", id: newId });
+    return new Response(
+      JSON.stringify({ message: "Reserva creada", id: newId }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
   } catch (error) {
     console.error("Error al insertar reserva:", error);
-    return res.status(500).json({ message: "Error en el servidor" });
+    return new Response(JSON.stringify({ message: "Error en el servidor" }), {
+      status: 500,
+    });
   }
-}
+};
