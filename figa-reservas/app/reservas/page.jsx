29dc7e2218bc2dfa1b28d603487f843197ea6771
dapxdra <da.araya98@gmail.com>
2025-05-06 18:15:48 -0600

    auth de las rutas

--- figa-reservas/app/reservas/page.jsx
+++ figa-reservas/app/reservas/page.jsx
@@ -1,6 +1,22 @@
 import ReservaForm from "../components/reservaform.jsx";
+import { useEffect, useState } from "react";
+import { auth } from "../lib/firebase.jsx";
+import { useRouter } from "next/navigation";
 
 export default function ReservasPage() {
+  const router = useRouter();
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (!user) {
+        router.push("/login"); // Redirigir si no está autenticado
+      } else {
+        setUser(user);
+      }
+    });
+    return () => unsubscribe(); // Limpiar el listener al desmontar el componente
+  }, [router]);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-black bg-white p-4">
       <ReservaForm />
